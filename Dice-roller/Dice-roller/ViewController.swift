15bee65900e2285a3765b43789ed7c9444f59e21
Reehan9 <syedreehan998@gmail.com>
2022-10-10 00:02:30 +0530

    Intial commit

---
+++ Dice-roller/Dice-roller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Dice-roller
+//
+//  Created by Syed Reehan on 18/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dieImg: UIImageView!
+    @IBOutlet weak var dieImg2: UIImageView!
+    
+
+    @IBOutlet weak var roll: UIButton!
+    
+    @IBAction func roll(_ sender: Any) {
+        var a = UIImage(imageLiteralResourceName: "die-6.jpg")
+        var b = UIImage(imageLiteralResourceName: "die-1.jpg")
+        var c = UIImage(imageLiteralResourceName: "die-2.jpg")
+        var d = UIImage(imageLiteralResourceName: "die-3.jpg")
+        var e = UIImage(imageLiteralResourceName: "die-5.jpg")
+        var f =  UIImage(imageLiteralResourceName: "die-4.jpg")
+        dieImg.image = [a,b,c,d,e,f][Int.random(in: 0...5)]
+        dieImg2.image = [a,b,c,d,e,f][Int.random(in: 0...5)]
+      }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Dice-Roller
+
+<h2> A dice roller application which generates random images of the dice when the roll button is pressed </h2>
+
+<p align="center">
+
+![output](https://user-images.githubusercontent.com/92086803/194761303-b87d6193-5a31-4471-a45c-565aa7544bd2.gif)
+
+</p>
