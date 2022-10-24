be5d3875cc12b77250d60f390c804e0fbae6986e
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-10-24 09:44:27 +0530

    Bug Fixed
    Changed var to 'let'

--- Dice-roller/Dice-roller/ViewController.swift
+++ Dice-roller/Dice-roller/ViewController.swift
@@ -16,12 +16,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var roll: UIButton!
     
     @IBAction func roll(_ sender: Any) {
-        var a = UIImage(imageLiteralResourceName: "die-6.jpg")
-        var b = UIImage(imageLiteralResourceName: "die-1.jpg")
-        var c = UIImage(imageLiteralResourceName: "die-2.jpg")
-        var d = UIImage(imageLiteralResourceName: "die-3.jpg")
-        var e = UIImage(imageLiteralResourceName: "die-5.jpg")
-        var f =  UIImage(imageLiteralResourceName: "die-4.jpg")
+        let a = UIImage(imageLiteralResourceName: "die-6.jpg")
+        let b = UIImage(imageLiteralResourceName: "die-1.jpg")
+        let c = UIImage(imageLiteralResourceName: "die-2.jpg")
+        let d = UIImage(imageLiteralResourceName: "die-3.jpg")
+        let e = UIImage(imageLiteralResourceName: "die-5.jpg")
+        let f = UIImage(imageLiteralResourceName: "die-4.jpg")
         dieImg.image = [a,b,c,d,e,f][Int.random(in: 0...5)]
         dieImg2.image = [a,b,c,d,e,f][Int.random(in: 0...5)]
       }
